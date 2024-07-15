38382578fa33d849d0bf6901e02dbe40dae792d9
kiquetal <kiquetal@gmail.com>
2024-07-15 09:55:28 -0400

    Adding new message mapper

--- backend/src/main/java/me/cresterida/ExceptionOidc.java
+++ backend/src/main/java/me/cresterida/ExceptionOidc.java
@@ -1,13 +1,13 @@
 package me.cresterida;
 
-import io.quarkus.oidc.OIDCException;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.quarkus.security.AuthenticationFailedException;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.ExceptionMapper;
 import jakarta.ws.rs.ext.Provider;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @Provider
 public class ExceptionOidc implements ExceptionMapper<AuthenticationFailedException>
@@ -15,9 +15,19 @@ public class ExceptionOidc implements ExceptionMapper<AuthenticationFailedExcept
     @Override
     public Response toResponse(AuthenticationFailedException exception) {
         System.out.println("ExceptionOidc: " + exception.toString());
-        Map<String,String> response= new HashMap<>();
-        response.put("error", "AuthenticationFailedException");
 
-        return Response.status(Response.Status.UNAUTHORIZED).entity(response).build();
+        ErrorResponse response = new ErrorResponse();
+        response.error = "AuthenticationFailedException";
+        response.message = exception.getMessage();
+        return Response.status(Response.Status.UNAUTHORIZED).entity(response)
+                .type(MediaType.APPLICATION_JSON_TYPE)
+                .build();
+    }
+    private static class ErrorResponse {
+        @JsonProperty
+        public String error;
+       @JsonProperty
+        public String message;
+
     }
 }
